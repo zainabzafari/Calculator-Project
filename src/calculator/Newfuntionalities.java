6ddc9d5bdce3501a0885421559aec8de0c64afb8
zainabzafari <zainabzafari1393@gmail.com>
2023-11-27 13:20:44 +0100

    added new class

--- src/calculator/Newfuntionalities.java
+++ src/calculator/Newfuntionalities.java
@@ -1,10 +1,11 @@
 package calculator;
 
-public class Newclass {
+public class Newfuntionalities {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("I will add new functionalities to this project");
 	}
 
 }
